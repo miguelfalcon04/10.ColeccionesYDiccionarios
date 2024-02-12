2b354748e6be8d8211c5c049b70bc71879549ae8
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-02-12 08:23:30 +0100

    commit

---
+++ Ejercicios/Eje15/Producto.java
@@ -0,0 +1,44 @@
+package Ejercicios.Eje15;
+
+public class Producto {
+
+    private String nombre;
+    private int cantidad;
+    private double precio;
+    private double total;
+
+    public Producto(String nom, int cant, double prec){
+        this.nombre = nom;
+        this.cantidad = cant;
+        this.precio = prec;
+        this.total = (precio*(double)cantidad);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-8s %5.2f %5d %10.2f", nombre, precio, cantidad, total);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+}
